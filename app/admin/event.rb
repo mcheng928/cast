3200a6bb187fa16d224de88491e8d18c75a2c02e
Raghu Betina <raghu@firstdraft.com>
2016-11-29 15:03:21 +0000

    Whitelist columns in Event for ActiveAdmin

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Event do
 
+ permit_params :host_id, :start_datetime, :end_datetime
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
