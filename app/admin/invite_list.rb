0c403c80e1f9cc4147968dc13bc4a55c0ded7e2a
Raghu Betina <raghu@firstdraft.com>
2016-11-29 15:03:38 +0000

    Whitelist columns in InviteList for ActiveAdmin

--- app/admin/invite_list.rb
+++ app/admin/invite_list.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register InviteList do
 
+ permit_params :event_id, :invitee_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
